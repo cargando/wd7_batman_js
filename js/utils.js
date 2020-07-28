d89cf8515a2efd475e00584685486b6472bd5261
sergey_spynu <sspynu@tsconsulting.com>
2020-07-28 21:18:18 +0300

    fetch data

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,27 @@
 
+
+
+document.getElementById('searchButton').addEventListener('click', searchShow);
+
+function searchShow() {
+
+  const text = getSearchText().trim();
+  if (!text.length) {
+    return;
+  }
+
+  const fetchResult = fetch(`https://api.tvmaze.com/search/shows?q=${ text }`);
+
+  fetchResult.then(response => {
+    return response.json();
+  }).then( data => {
+    // получили данные с сервака
+    console.log('DATA from SERVER:', data)
+  });
+
+}
+
+
+function getSearchText() {
+  return document.getElementById('searchText').value;
+}
